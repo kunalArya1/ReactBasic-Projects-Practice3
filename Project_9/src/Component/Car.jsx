f5243e047fdff150f69c77e91a8d8be33741200f
kunalArya1 <kunalkrraj220@gmail.com>
2024-06-06 10:19:42 +0530

    Project 10 Created

--- Project_9/src/Component/Car.jsx
+++ Project_9/src/Component/Car.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+/**  
+ 
+9. Given an array of objects representing a list of cars. Each object consists of: id, name, price, category. Build a React component that shows on the DOM the total number of cars present in each category. For Example: if there are categories, luxury and sports, where there are 5 luxury cars and 2 sports car then the data should be represented as:
+ luxury: 5
+ sports: 2
+
+
+  */
+
 const Car = ({ data }) => {
   return (
     <div className=" text-center">
